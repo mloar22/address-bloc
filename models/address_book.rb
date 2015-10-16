1e99851390eb2e053ab22f9f8dbfd1be7727e33c
Mitch Loar <mitchloarweb@gmail.com>
2015-10-16 14:09:22 -0700

    Checkpoint 25 Completed, CSV's

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,38 +1,31 @@
-require_relative "./entry.rb"
- 
+require_relative "entry.rb"
+ require "csv"
+
  class AddressBook
    attr_accessor :entries
 
    def initialize
-     @entries = [
-      Entry.new("Michael", "12321", "michael@example.com"),
-      Entry.new("Example1", "12321312", "Example1@example.com"),
-      Entry.new("Example2", "12321312", "Example2@example.com")
-     ]
+     @entries = []
    end
-   
+
    def add_entry(name, phone, email)
- 
      index = 0
      @entries.each do |entry|
- 
        if name < entry.name
          break
        end
        index += 1
      end
- 
      @entries.insert(index, Entry.new(name, phone, email))
    end
 
-   def remove_entry(entry_to_delete)
-    #this needs to remove an entry from @entries
-    #remove a specific entry
+   def import_from_csv(file_name)
+     csv_text = File.read(file_name)
+     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+     csv.each do |row|
+       row_hash = row.to_hash
+       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+     end
 
-    @entries.each do |entry|
-      if entry == entry_to_delete
-        @entries.delete(entry)
-      end
-    end
    end
  end
